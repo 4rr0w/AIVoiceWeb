55ada4af651df332b320fb84915e11b20a8342f1
himanshu <himanshu@buffercode.in>
2024-06-17 02:12:03 +0530

    initial draft

---
+++ constants/index.js
@@ -0,0 +1,96 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "Natural Sounding Voice",
+    content:
+      "Our AI voice over service provides natural-sounding voice overs in multiple languages, perfect for training videos, marketing content, and more. With our technology, you can create professional voice overs without the need for recording studios.",
+  },
+  {
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "Multiple Languages",
+    content:
+      "Ensures accurate translation and localization. Customizable accents and regional pronunciations to cater to diverse audiences.",
+  },
+  {
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Lifelike Appearance",
+    content:
+      "Our AI-built real avatars bring a human touch to your videos. These avatars can speak multiple languages and are ideal for creating engaging, personalized video content for your global audience.",
+  },
+  {
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "Accurate lip-syncing",
+    content:
+      "Avatars can speak multiple languages fluently. Supports a variety of accents and regional dialects. Regional accents and colloquialisms for authentic communication",
+  },
+  {
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "Realistic movements and gestures",
+    content:
+      "Customized content to fit cultural nuances and preferences. Tailored visuals and graphics to resonate with diverse audiences.",
+  },
+];
+
+export const startingFeatures = [
+  "Draft an email to mail@email.com",
+  "We will get back to you within 24 hours",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "A new world",
+    subtitle:
+      "we have the latest update with new world for you to try never mind",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "More realistic",
+    subtitle:
+      "In the latest update, your eyes are narrow, making the world more realistic than ever",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/planet-06.png",
+    title: "The launch of the Metaverse and be happy :)",
+    subtitle:
+      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
+  },
+  {
+    imgUrl: "/planet-07.png",
+    title: "7 tips to easily master the madness of the Metaverse",
+    subtitle:
+      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
+  },
+  {
+    imgUrl: "/planet-08.png",
+    title: "With one platform you can explore the whole world virtually",
+    subtitle:
+      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+    link: "https://twitter.com/",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+    link: "https://www.linkedin.com/",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+    link: "https://www.instagram.com/",
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="| About Us"
+        textStyles="text-center"
+      />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold"> We are a cutting-edge tech company specializing in AI voice over and real avatars. Our mission is to revolutionize video content creation by providing seamless and natural-sounding voice overs along with highly realistic AI-generated avatars. Our solutions cater to businesses looking to enhance their video content and reach a global audience effortlessly.
+        </span>
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
