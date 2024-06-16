5b6ed5b5010afd6f77c96bbf07233b1954d2e77e
himanshu <himanshu@buffercode.in>
2024-06-17 02:17:30 +0530

    fix error

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import styles from '../styles';
+// import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick, content = '' }) => (
